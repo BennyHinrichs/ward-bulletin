ac59357edcf4fa3d425ae0fb11c183af6543457a
BennyHinrichs <hinrichs.benny@gmail.com>
2024-02-17 18:05:43 -0700

    add netlify config

--- app/remix.config.cjs
+++ app/remix.config.cjs
@@ -1,8 +1,11 @@
+import { config } from '@netlify/remix-adapter';
+
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   ignoredRouteFiles: ['**/.*'],
+  ...(process.env.NODE_ENV === 'production' ? config : undefined),
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-}
+};
